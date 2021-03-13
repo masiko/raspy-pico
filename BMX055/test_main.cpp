9c7aa80efb300283b160cbaa35133296439e0b50
masiko <masiko.taiitirou@gmail.com>
2021-03-13 12:26:52 +0900

    Added noise countermeasures and angle calculation

--- BMX055/test_main.cpp
+++ BMX055/test_main.cpp
@@ -14,15 +14,46 @@ int main() {
 	// LCD setting	
 	bmx055::bmx055 imu(i2c0);
 
-	double accl[3] = {1.0, 2.0, 3.0};
-	double gyro[3] = {-1.0, -2.0, -3.0};
-	double temp = 0.1234;
+	double accl[3];
+	double gyro[3];
+	double temp;
+	double gyro_past[3] = {0.0,0.0,0.0};
+	double gyro_delta[3];
+	double gyro_delta_integra[3] = {0.0,0.0,0.0};
+	double gyro_offset[3];
+	double angle[3] = {0.0,0.0,0.0};
 	
+	sleep_ms(5000);
+		
+	for (int i=0; i<200; i++) {
+		imu.readData();
+		imu.getData(accl, gyro, &temp);
+		for (int j=0; j<3; j++) {
+			gyro_delta[j] = gyro[j] - gyro_past[j];
+			gyro_delta_integra[j] += gyro_delta[j];
+			gyro_offset[j] += gyro_delta_integra[j];
+			gyro_past[j] = gyro[j];
+		}
+		sleep_ms(100);
+	}
+
+	for (int i=0; i<3; i++) {
+		gyro_delta_integra[i] += -gyro_offset[i] / 200;
+	}
+
 	while(1) {
 		imu.readData();
 		imu.getData(accl, gyro, &temp);
+		for (int i=0; i<3; i++) {
+			gyro_delta[i] = gyro[i] - gyro_past[i];
+			gyro_delta_integra[i] += gyro_delta[i];
+			angle[i] += gyro_delta_integra[i]*0.1;
+			gyro_past[i] = gyro[i];
+		}
 		printf("%f,%f,%f,%f,%f,%f,%f\n", 
-			accl[0], accl[1], accl[2], gyro[0], gyro[1], gyro[2], temp);
+			angle[0], angle[1], angle[2], gyro[0], gyro[1], gyro[2], temp);
+		/*printf("%f,%f,%f,%f,%f,%f,%f\n", 
+			accl[0], accl[1], accl[2], gyro[0], gyro[1], gyro[2], temp);*/
 		sleep_ms(100);
 	}
 	return 0;
